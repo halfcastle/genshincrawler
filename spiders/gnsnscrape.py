2847102989c424166e34d31e1525d284b265d667
brp <86597708+halfcastle@users.noreply.github.com>
2022-05-17 17:19:40 -0700

    scrapyscrape

---
+++ spiders/gnsnscrape.py
@@ -0,0 +1,16 @@
+import scrapy
+class gnsnspi(scrapy.Spider):
+    name = "gnsnspi"
+    def parse(self, response):
+        chars = response.css("div.char_sea_cont a")
+        for char in [a.attrib["href"] for a in chars]:
+            char_url = response.urljoin(char)
+            yield scrapy.Request(char_url, callback = self.parse_characters)
+    def parse_characters(self, response):
+        name = response.css("div.custom_title::text").get()
+        serifu_block = response.css("table.item_main_table")
+        serifu = serifu_block.css("td::text").getall()
+        yield{
+            "name": name,
+            "lines": serifu
+        }
